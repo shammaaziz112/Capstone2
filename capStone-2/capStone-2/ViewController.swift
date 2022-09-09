89326b4a49e92c0945d1f777a812ae3e7d92091e
AlenaziHazal <roomyham@MacBook-Pro.local>
2022-09-09 18:31:47 +0300

    Test hazal

---
+++ capStone-2/capStone-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  capStone-2
+//
+//  Created by AlenaziHazal on 13/02/1444 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
