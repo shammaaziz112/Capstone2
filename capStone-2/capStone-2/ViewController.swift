9bc8e1d6a34ca97a954acc30cf79a43403ed640b
AlenaziHazal <roomyham@MacBook-Pro.local>
2022-09-09 19:15:37 +0300

    Hazal second test

--- capStone-2/capStone-2/ViewController.swift
+++ capStone-2/capStone-2/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         // Hazal test merge
+        // Hazal Second 
     }
 
 
