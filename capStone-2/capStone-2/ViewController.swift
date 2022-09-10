ba9c8bbdd503ff905e1ea2adaba3ea9a78942318
Hessa <azizalkhadi@MBPalkhsbHessa4.lan>
2022-09-10 16:21:17 +0300

    finish requirement 3

--- capStone-2/capStone-2/ViewController.swift
+++ capStone-2/capStone-2/ViewController.swift
@@ -8,77 +8,81 @@
 import UIKit
 import CoreLocation
 class ViewController: UIViewController, CLLocationManagerDelegate {
-    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var collectionViewFlickr: UICollectionView!
+
     var locationManeger = CLLocationManager()
-    var myImage:Data?
+//    var myImage:Data?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        collectionViewFlickr.register(UINib(nibName: "CollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "cell")
         // Do any additional setup after loading the view.
         // Hazal test merge
         // Hazal Second
         locationManeger.delegate = self
         locationManeger.requestAlwaysAuthorization()
         locationManeger.startUpdatingLocation()
-        fetchImage()
-        print("bahbja")
-    }
-    func fetchImage(){
-        //Step1
-        // Take the String URl for filker
-        let StringUrl = "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=372018c5205cf1fca8a86006716de2f8&lat=24.774265&lon=46.738586&radius=10&format=json&nojsoncallback=1&auth_token=72157720856612035-7f702afff41e1783&api_sig=41075711e4565289e5f615392b6e0145"
-        //Step2
-        // Conver StringURL to URL
-        guard let Url = URL(string: StringUrl) else {
-            print("URl string is nil")
-            return
-        }
-        //Step3
-        //Take the URL to session
-        let Image = URLSession.shared.dataTask(with: Url) { data, responce, erorr in
-            guard erorr == nil else{
-                print("Erorr: \(erorr!.localizedDescription)")
-                return
-            }
-            guard let responce = responce as? HTTPURLResponse else {
-                print("responce: is nil")
-                return
-            }
-            guard responce.statusCode >= 200
-                    && responce.statusCode <= 299 else {
-                print("Statuse Code Should be 2xx, Butthe code is \(responce.statusCode)")
-                return
-            }
-            //if we want to read json NOT MATTER
-           // let jsonString = String(data: data!, encoding: .utf8)
-           // print("Jsone:\(jsonString!)")
-            
-            //Step4
-            //Decode json from the MODEL from the Navigator in the Left
-            guard let json = try? JSONDecoder().decode(Welcome.self, from: data!) else {
-                print("Json Error")
-                return
-            }
-            //Step5
-            // I Finished the Requires number 2
-            let omage2 = "https://live.staticflickr.com/\(json.photos.photo[2].server)/\(json.photos.photo[2].id)_\(json.photos.photo[2].secret).jpg"
-            //We need to put all the omgae2 above in array and appeaned them becosue the image dosn't have long and lat tude in the URL we take so its hard to inplement all the lat and long one by one (Think if you can do it) otherwise i do the Requires
-            
-            //change the stringImage to urlData
-            guard let url1 = URL(string: omage2) else {print("imageErorr")
-                return}
-            //change the urlData to Data for present it
-            guard let data1 = try? Data(contentsOf: url1) else {return}
-            //Put the data we take to the varable
-            self.myImage = data1
-            //use Dispatch to raech main async to show th eimage we take from url 
-            DispatchQueue.main.async {
-                self.imageView.image = UIImage(data: self.myImage!)
-            }
-         
-        }
         
-        Image.resume()
+        print("bahbja")
     }
 
 }
 
+extension ViewController: UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
+            
+        let StringUrl = "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=aaa2a4aa90e3f5fb291032faa5f2c690&lat=24.774265&lon=46.738586&radius=10&format=json&nojsoncallback=1&auth_token=72157720856705390-dad22851d0976dc0&api_sig=7a8db46f8f689f8e4666b1c51a6f13b4"
+        
+        cell.cellImage.loadImageWithCashe(StringUrl, indexPath: indexPath.row)
+        
+        return cell
+    }
+}
+
+var imageToCashe = NSCache<NSString, UIImage>()
+extension UIImageView{
+    
+    func loadImageWithCashe(_ urlString: String, indexPath: Int){
+        
+        self.image = nil
+        
+        if let cacheImage = imageToCashe.object(forKey: NSString(string: urlString)) {
+            self.image = cacheImage
+        }
+        
+        guard let url = URL(string: urlString) else { return }
+        
+        let Image = URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error = error {
+                print(error)
+            }
+            
+            guard let json = try? JSONDecoder().decode(Welcome.self, from: data!) else {
+                print("Json Error")
+                return
+            }
+            
+            let omage2 = "https://live.staticflickr.com/\(json.photos.photo[indexPath].server)/\(json.photos.photo[indexPath].id)_\(json.photos.photo[indexPath].secret).jpg"
+            
+            guard let url1 = NSURL(string: omage2) else {print("imageErorr")
+                return}
+            
+            guard let data1 = try? Data(contentsOf: url1 as URL) else {return}
+            
+            if let downloadImage = UIImage(data: data1){
+                DispatchQueue.main.async {
+                    self.image = downloadImage
+                    imageToCashe.setObject(downloadImage, forKey: NSString(string: urlString))
+                }
+            }
+        }
+        Image.resume()
+    }
+}
